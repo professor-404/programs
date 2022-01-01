961ab2c3e53ca4620c70b21bf73f0b0e0fab2ad5
Mudassir-1 <96912402+Mudassir-1@users.noreply.github.com>
2022-01-01 22:03:51 +0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,88 +1,179 @@
 #include<iostream>
-#include<conio.h>
-#include<string>
 #include<iomanip>
 using namespace std;
 int main()
- {
-	
-
-	//--------------------------------PALINDROME NUMBERS--------------------------
-
-	/*int num, rem, x, rev = 0;
-	cout << "Enter any number to check palindrome : ";
-	cin >> num;
-	x = num;
-	while (x > 0)
+{
+	//----------------LAb 8 Task 1------------
+	/*int ASCII,a=1,b=16,mul;
+	for (ASCII = 0; ASCII <= 127; ASCII++)
 	{
-		rem = x % 10;
-		rev = (rev * 10) + rem;
-		x /= 10;
-	}
-	if (rev == num)
-		cout << "Number is Palindrome";
-	else
-		cout << "Number is not a Palindrome";*/
+		cout << (char)ASCII<<"\t";
+		if (ASCII == 16 || ASCII == 32 || ASCII == 48 || ASCII == 64 || ASCII == 80 || ASCII == 96 || ASCII == 112)
+			cout << "\n\n";
+			if(i%16==0)
+			cout<<endl;*/
+			/*}*/
+
+			//-------------------Task2--------------
+
+			/*float a = 1.0;
+			float inc = 1;
+			float sum = 0;
+			while (inc <= 45)
+			{		sum  += (1 / (inc));
+			inc++;
+			}
+			cout << "SUM = " << sum;*/
+
+			//------------------------Task 3---------------
+
+			//------------------------a part-------------------------
+
+			/*int num;
+			for (num = 1; num <= 100 ; num++)
+				cout << num << " ";*/
+
+				//------------------------b part------------------------
+
+				/*int num, a;
+				cout << "Enter +ve Integer ";
+				cin >> num;
+				for (a = 1; a <= num; a++)
+					cout << a << " ";*/
+
+					//----------------------c part---------------------------
 
 
-	//--------------------------------PALINDROME STRING--------------------------
+					/*char c;
+					for (c = 'Z'; c >= 'A'; c--)
+						cout << c << " ";*/
 
-	/*string str;
-	int x, y, z = 0;
-	cout << "Enter string to check Palindrome ";
-	cin >> str;
-	int n = str.size();
-	for (x = 0, y = n - 1; x < y; x++, y--)
+						//-------------------------d part-----------------------
+
+						/*char c;
+						for (c = 'Z'; c >= 'A'; c--)
+						{
+							if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+								continue;
+							cout << c << " ";
+						}*/
+
+						//-------------------------e part---------------------
+
+						/*int x, y,a;
+						cout << "Enter strting value ";
+						cin >> x;
+						cout << "Enter Ending Value ";
+						cin >> y;
+						for (a = x; a <= y; a++)
+							cout << a << " ";*/
+
+							//-------------------------Task5---------------------------
+							//-------------------------a part--------------------------
+							/*int i = 0, j;
+							while (i <= 10)
+							{
+								j = 0;
+								while (j <= i)
+								{
+									cout << "*";
+									j++;
+								}
+								i++;
+								cout << "\n";
+							}*/
+
+							//-------------------------b part-------------------------
+
+							/*int i = 10, j;
+							while (i >= 1)
+							{
+								j = 0;
+								while (j < i)
+								{
+									cout << "*";
+									j++;
+								}
+								i--;
+								cout << "\n";
+							}*/
+
+							//------------------------------c part------------------------
+
+							/*int i, j, k;
+							for (i = 10; i >= 1; i--)
+							{
+								for (k = 10 - i; k > 0; k--)
+									cout << " ";
+								for (j = 1; j <= i; j++)
+								{
+									cout << "*";
+								}
+								cout << "\n";
+							}*/
+
+
+							//------------------------------d part------------------------
+
+							/*int i, j, k;
+							for (i = 1; i <= 10; i++)
+							{
+								for (k = 10 - i; k > 0; k--)
+									cout << " ";
+								for (j = 1; j <= i; j++)
+								{
+									cout << "*";
+								}
+								cout << "\n";
+							}
+							return 0;*/
+
+							//---------------------------Task 6-------------------------
+
+							/*for (int i = 0; i <= 10; i++)
+								cout << "*";
+							cout << endl;
+							for (int i = 0; i <= 4; i++)
+								cout << "*" << 0;
+							cout << "*"<<endl;
+							for (int i = 0; i <= 10; i++)
+								cout << "*";
+							cout << endl;
+							for (int i = 0; i <= 4; i++)
+								cout << "*"<<0;
+							cout << "*";
+							cout << endl;
+							for (int i = 0; i <= 10; i++)
+								cout << "*";*/
+
+//-----------------------------add matrix---------------------------
+
+	int matrix1[3][2];
+	int matrix2[3][2];
+	for (int r = 0; r < 3; r++)
 	{
-		if (str[x] != str[y])
+		for (int c = 0; c < 2; c++)
 		{
-			z = 0;
-			break;
+			cout << "Enter matrix1 value: [" << r << "][" << c << "] ";
+			cin >> matrix1[r][c];
 		}
-		else
-			z = 1;
 	}
-	if (z)
-		cout << "String is Palindrome";
-	else
-		cout << "String is not a Palindrome";*/
-
-
-	/*char name[100];
-	cout << "Enter string ";
-	cin >> name;
-	int i = 0, j = strlen(name) - 1;
-	bool x = true;
-	for (i = 0; i < j; i++)
+	for (int r = 0; r < 3; r++)
 	{
-		if (name[i] != name[j])
-			x = false;
-		j--;
+		for (int c = 0; c < 2; c++)
+		{
+			cout << "Enter matrix2 value: [" << r << "][" << c << "] ";
+			cin >> matrix2[r][c];
+		}
 	}
-	if(x)
-		cout << "String is Palindrome";
-	else
-		cout << "String is not a Palindrome";*/
-
-	//--------------------------------ARMSTRONG--------------------------
-	
-	/*int num, n, rem, x = 0;
-	cout << "Enter number to check armstrong ";
-	cin >> num;
-	n = num;
-	while (n > 0)
+	for (int r = 0; r < 3; r++)
 	{
-		rem = n % 10;
-		x += pow(rem, 3);
-		n /= 10;
+		for (int c = 0; c < 2; c++)
+		{
+			cout << matrix1[r][c] + matrix2[r][c];
+			cout << " ";
+		}
+		cout << endl;
 	}
-	if (num == x)
-		cout << "Number is Armstrong ";
-	else
-		cout << "Number is not an Armstrong";*/
-
-
-	
 	return 0;
-
 }
